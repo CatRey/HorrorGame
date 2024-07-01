2d4ab19257c59b8186175d2b051e63b9978d69dd
Merlok_3_0 <egor.soshkin1759@gmail.com>
2024-07-01 21:50:43 +0300

    added lever pulling minigame

---
+++ Assets/Minigames/Lever Puller/MinigameLeverPulling.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinigameLeverPulling : Minigame
+{
+    public Button[] levers;
+    public Color turnedOnColor, turnedOffColor;
+    public bool[] turnedOn;
+    
+    private void Start()
+    {
+        turnedOn = new bool[levers.Length];
+    }
+
+
+    private void Update()
+    {
+        
+    }
+
+    public void SwitchLever(int index)
+    {
+        turnedOn[index] = !turnedOn[index];
+
+        levers[index].image.color = turnedOn[index] ? turnedOnColor : turnedOffColor;
+        levers[index].GetComponentInChildren<Text>().text = turnedOn[index] ? "ON" : "OFF";
+
+        if (Array.TrueForAll(turnedOn, x => x))
+        {
+            invoker.MinigameStopped();
+        }
+    }
+}
