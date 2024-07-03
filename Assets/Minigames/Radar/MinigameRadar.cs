fe48a0cfa72364edc68359bf3f5f0a27754464b4
Merlok_3_0 <egor.soshkin1759@gmail.com>
2024-07-04 01:26:54 +0300

    added visual effects for radar

--- Assets/Minigames/Radar/MinigameRadar.cs
+++ Assets/Minigames/Radar/MinigameRadar.cs
@@ -5,11 +5,10 @@ using UnityEngine.UI;
 
 public class MinigameRadar : Minigame
 {
-    public RawImage scene;
 
     
     private void Start()
     {
-        scene.texture = GameObject.FindGameObjectWithTag("2DSceneRenderer").GetComponent<Camera>().targetTexture;
+
     }
 }
