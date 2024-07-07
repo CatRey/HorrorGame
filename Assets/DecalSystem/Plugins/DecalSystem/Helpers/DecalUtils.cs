243d59ef9d069dbb8a41d51bed8fa7404080b427
Merlok_3_0 <egor.soshkin1759@gmail.com>
2024-07-07 06:54:32 +0300

    added decals/wall cracks, sillhouettes now spawn by ones

---
+++ Assets/DecalSystem/Plugins/DecalSystem/Helpers/DecalUtils.cs
@@ -0,0 +1,161 @@
+﻿namespace DecalSystem {
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using UnityEngine;
+    using UnityEngine.Rendering;
+
+    static class DecalUtils {
+
+
+        public static MeshFilter[] GetAffectedObjects(Decal decal) {
+            var bounds = GetBounds( decal );
+            var isOnlyStatic = decal.gameObject.isStatic;
+
+            var a = GameObject.FindObjectsOfType<MeshRenderer>();
+            var b = a.Where(i => i.GetComponent<Decal>() == null); // ignore another decals
+            var c = b.Where(i => i.gameObject.isStatic || !isOnlyStatic);
+            var d = c.Where(i => HasLayer(decal.LayerMask, i.gameObject.layer));
+            var e = d.Where(i => bounds.Intersects(i.bounds));
+
+            var f = e.Select(i => i.GetComponent<MeshFilter>());
+            var g = f.Where(i => i);
+
+
+            return g
+                
+                
+                
+                
+
+                
+                
+                
+                
+                .ToArray();
+        }
+
+        public static Terrain[] GetAffectedTerrains(Decal decal) {
+            var bounds = GetBounds( decal );
+            var isOnlyStatic = decal.gameObject.isStatic;
+
+            return Terrain.activeTerrains
+                .Where( i => i.gameObject.isStatic || !isOnlyStatic )
+                .Where( i => HasLayer( decal.LayerMask, i.gameObject.layer ) )
+                .Where( i => bounds.Intersects( i.GetBounds() ) )
+                .ToArray();
+        }
+
+
+        public static Bounds GetBounds(Decal decal) {
+            var transform = decal.transform;
+            var size = transform.lossyScale;
+            var min = -size / 2f;
+            var max = size / 2f;
+
+            var vts = new Vector3[] {
+                new Vector3( min.x, min.y, min.z ),
+                new Vector3( max.x, min.y, min.z ),
+                new Vector3( min.x, max.y, min.z ),
+                new Vector3( max.x, max.y, min.z ),
+
+                new Vector3( min.x, min.y, max.z ),
+                new Vector3( max.x, min.y, max.z ),
+                new Vector3( min.x, max.y, max.z ),
+                new Vector3( max.x, max.y, max.z ),
+            };
+
+            vts = vts.Select( transform.TransformDirection ).ToArray();
+            min = vts.Aggregate( Vector3.Min );
+            max = vts.Aggregate( Vector3.Max );
+
+            return new Bounds( transform.position, max - min );
+        }
+
+        private static Bounds GetBounds(this Terrain terrain) {
+            var bounds = terrain.terrainData.bounds;
+            bounds.center += terrain.transform.position;
+            return bounds;
+        }
+
+
+        public static void SetDirty(Decal decal) {
+/*            if (decal.gameObject.scene.IsValid()) {
+                if (!EditorApplication.isPlaying) EditorSceneManager.MarkSceneDirty( decal.gameObject.scene );
+            } else {
+                EditorUtility.SetDirty( decal.gameObject );
+            }*/
+        }
+
+
+        public static void FixRatio(Decal decal, ref Vector3 oldScale) {
+            var transform = decal.transform;
+            var rect = decal.Sprite.rect;
+            var ratio = rect.width / rect.height;
+
+            var scale = transform.localScale;
+            FixRatio( ref scale, ref oldScale, ratio );
+
+            var hasChanged = transform.hasChanged;
+            transform.localScale = scale;
+            transform.hasChanged = hasChanged;
+        }
+
+        private static void FixRatio(ref Vector3 scale, ref Vector3 oldScale, float ratio) {
+            if (!Mathf.Approximately( oldScale.x, scale.x )) scale.y = scale.x / ratio; // if scale.x was changed then fix scale.y
+            else
+            if (!Mathf.Approximately( oldScale.y, scale.y )) scale.x = scale.y * ratio;
+            else
+            if (!Mathf.Approximately( scale.x / scale.y, ratio )) scale.x = scale.y * ratio;
+
+            oldScale = scale;
+        }
+
+
+        // Helpers
+        private static bool HasLayer(LayerMask mask, int layer) {
+            return (mask.value & 1 << layer) != 0;
+        }
+
+        public static Mesh MakeReadableMeshCopy(Mesh nonReadableMesh)
+        {
+            Mesh meshCopy = new Mesh();
+            meshCopy.indexFormat = nonReadableMesh.indexFormat;
+
+            // Handle vertices
+            GraphicsBuffer verticesBuffer = nonReadableMesh.GetVertexBuffer(0);
+            int totalSize = verticesBuffer.stride * verticesBuffer.count;
+            byte[] data = new byte[totalSize];
+            verticesBuffer.GetData(data);
+            meshCopy.SetVertexBufferParams(nonReadableMesh.vertexCount, nonReadableMesh.GetVertexAttributes());
+            meshCopy.SetVertexBufferData(data, 0, 0, totalSize);
+            verticesBuffer.Release();
+
+            // Handle triangles
+            meshCopy.subMeshCount = nonReadableMesh.subMeshCount;
+            GraphicsBuffer indexesBuffer = nonReadableMesh.GetIndexBuffer();
+            int tot = indexesBuffer.stride * indexesBuffer.count;
+            byte[] indexesData = new byte[tot];
+            indexesBuffer.GetData(indexesData);
+            meshCopy.SetIndexBufferParams(indexesBuffer.count, nonReadableMesh.indexFormat);
+            meshCopy.SetIndexBufferData(indexesData, 0, 0, tot);
+            indexesBuffer.Release();
+
+            // Restore submesh structure
+            uint currentIndexOffset = 0;
+            for (int i = 0; i < meshCopy.subMeshCount; i++)
+            {
+                uint subMeshIndexCount = nonReadableMesh.GetIndexCount(i);
+                meshCopy.SetSubMesh(i, new SubMeshDescriptor((int)currentIndexOffset, (int)subMeshIndexCount));
+                currentIndexOffset += subMeshIndexCount;
+            }
+
+            // Recalculate normals and bounds
+            meshCopy.RecalculateNormals();
+            meshCopy.RecalculateBounds();
+
+            return meshCopy;
+        }
+
+    }
+}
--- Assets/DoubleSidedCollision.cs
+++ Assets/DoubleSidedCollision.cs
@@ -3,50 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshCollider))]
+[RequireComponent(typeof(MeshCollider)), RequireComponent(typeof(MeshFilter))]
 public class DoubleSidedCollision : MonoBehaviour
 {
-    MeshCollider toFlip;
-    public MeshCollider flipped;
     
     private void Start()
     {
-        toFlip = GetComponent<MeshCollider>();
-        var copy = Instantiate(gameObject, transform, true);
-        copy.transform.SetAsFirstSibling();
-        flipped = copy.GetComponent<MeshCollider>();
-        flipped.enabled = true;
-
-        var copiedMesh = new Mesh();
-        copiedMesh.SetVertices(flipped.sharedMesh.vertices);
-        copiedMesh.SetUVs(0, flipped.sharedMesh.uv);
-        copiedMesh.SetUVs(1, flipped.sharedMesh.uv2);
-        copiedMesh.SetUVs(2, flipped.sharedMesh.uv3);
-        copiedMesh.SetUVs(3, flipped.sharedMesh.uv4);
-        copiedMesh.SetUVs(4, flipped.sharedMesh.uv5);
-        copiedMesh.SetUVs(5, flipped.sharedMesh.uv6);
-        copiedMesh.SetUVs(6, flipped.sharedMesh.uv7);
-        copiedMesh.SetUVs(7, flipped.sharedMesh.uv8);
-        copiedMesh.SetTriangles(flipped.sharedMesh.triangles, 0);
-        copiedMesh.SetTangents(flipped.sharedMesh.tangents);
-        copiedMesh.SetNormals(flipped.sharedMesh.normals);
-        copiedMesh.SetColors(flipped.sharedMesh.colors);
-
-        copiedMesh.SetIndices(copiedMesh.GetIndices(0).Concat(copiedMesh.GetIndices(0).Reverse()).ToArray(), MeshTopology.Triangles, 0);
-
-        flipped.sharedMesh = copiedMesh;
-
-        foreach (var item in copy.GetComponents<Component>())
-        {
-            if (item != flipped)
-            {
-                Destroy(item);
-            }
-        }
-
-        foreach (Transform child in copy.transform)
-        {
-            Destroy(child.gameObject);
-        }
+        var toFlip = GetComponent<MeshCollider>();
+        toFlip.sharedMesh.SetIndices(toFlip.sharedMesh.GetIndices(0).Concat(toFlip.sharedMesh.GetIndices(0).Reverse()).ToArray(), MeshTopology.Triangles, 0);
+        var toFlipVisual = GetComponent<MeshFilter>();
+        toFlipVisual.sharedMesh.SetIndices(toFlipVisual.sharedMesh.GetIndices(0).Concat(toFlipVisual.sharedMesh.GetIndices(0).Reverse()).ToArray(), MeshTopology.Triangles, 0);
     }
 }
