3d56c68e8301f20731ebb771a1e6b8ee6b0ba230
Merlok_3_0 <egor.soshkin1759@gmail.com>
2024-07-07 18:01:31 +0300

    added eating minigame, added decal meshes to files, so that they don't spend calculation time at runtime, (for some reason documents metadata was saved in this commit)

---
+++ Assets/Minigames/Eating/Eatable3D.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eatable3D : InteractableInCollider
+{
+    public Overlayable3D isSelected1, isSelected2;
+    public GameObject throwPrefab;
+    public float throwPower, hungerRestore;
+    public float goIntoSpeed;
+    bool eating;
+
+
+    private void Update()
+    {
+        if (!eating && (isSelected1.overlayed || isSelected2.overlayed))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                StartCoroutine(Eat(Vector3.Dot(Camera.main.transform.forward, transform.position - Camera.main.transform.position)));
+            }
+        }
+    }
+
+    public IEnumerator Eat(float wasDistance)
+    {
+        eating = true;
+        while (wasDistance > -2)
+        {
+            transform.position = Camera.main.transform.position + Camera.main.transform.forward * (wasDistance -= goIntoSpeed * Time.fixedDeltaTime);
+            yield return new WaitForFixedUpdate();
+        }
+
+        var can = Instantiate(throwPrefab);
+        can.transform.position = Camera.main.transform.position;
+        can.GetComponent<Rigidbody>().velocity = Camera.main.transform.forward * throwPower;
+
+        Destroy(gameObject);
+    }
+}
